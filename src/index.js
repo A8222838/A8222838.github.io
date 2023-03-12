d3b449147e7085846db5dea4d669a330ee059a75
A8222838 <A8222838@g.pccu.edu.tw>
2023-03-12 19:52:52 +0800

    fix path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter,Router, Route, Routes } from "react-router-dom";
 import About from "./pages/about";
 import HomePage from "./pages/homePage";
 import Projects from "./pages/projects";
@@ -8,12 +8,12 @@ import Layout from "./components/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/front-end'>
     <Layout>
       <Routes>
-        <Route exact path="/front-end/" element={<HomePage />} />
-        <Route exact path="/front-end/about/" element={<About />} />
-        <Route exact path="/front-end/projects/" element={<Projects />} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about/" element={<About />} />
+        <Route exact path="/projects/" element={<Projects />} />
       </Routes>
     </Layout>
   </BrowserRouter>
